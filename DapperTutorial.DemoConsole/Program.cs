b5cf999fb2b825934d55d2ad8f8864312cd53953
leandro-layerle <leandro.layerle@gmail.com>
2019-01-01 15:15:42 -0300

    Dapper Basic Read Method

--- DapperTutorial.DemoConsole/Program.cs
+++ DapperTutorial.DemoConsole/Program.cs
@@ -1,5 +1,10 @@
+using Dapper;
+using DapperTutorial.DemoConsole.Domain;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +15,23 @@ namespace DapperTutorial.DemoConsole
     {
         static void Main(string[] args)
         {
-            
+            BasicRead();
+            Console.ReadKey();
+        }
+
+        private static void BasicRead()
+        {
+            using (IDbConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DapperTutorialDB"].ConnectionString))
+            {
+                var sql = "SELECT * FROM dbo.Customer";
+
+                var customers = connection.Query<Customer>(sql).ToList();
+
+                customers.ForEach(customer =>
+                {
+                    Console.WriteLine($"{customer.Name} {customer.LastName}");
+                });
+            }
         }
     }
 }
