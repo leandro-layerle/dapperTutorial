cd3cc3cceca10b2d2532abac53e4855f07b06576
leandro-layerle <leandro.layerle@gmail.com>
2019-01-01 18:09:42 -0300

    Address Table Created Customer - Address FK Created

---
+++ DapperTutorialDB/dbo/Tables/Address.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[AddressId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Street] NVARCHAR(50) NULL, 
+    [Number] NCHAR(10) NULL
+)
--- DapperTutorialDB/dbo/Tables/Customer.sql
+++ DapperTutorialDB/dbo/Tables/Customer.sql
@@ -2,5 +2,7 @@
 (
 	[CustomerId] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Name] NVARCHAR(50) NULL, 
-    [LastName] NVARCHAR(50) NULL
+    [LastName] NVARCHAR(50) NULL, 
+    [AddressId] INT NULL, 
+    CONSTRAINT [FK_Customer_Address] FOREIGN KEY (AddressId) REFERENCES [Address](AddressId)
 )
